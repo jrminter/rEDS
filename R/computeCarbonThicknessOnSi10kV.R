fecde1c29df65d3589890e9bf2047ea2e9373027
John Minter <jrminter@gmail.com>
2016-11-18 14:23:08 -0500

    added C thickness computation functions at 5, 7, 10, and 20 kV

---
+++ R/computeCarbonThicknessOnSi10kV.R
@@ -0,0 +1,32 @@
+#' Compute the C thickness on Si at 10kV
+#'
+#' Use the results from a LOESS fit to results from DTSA-II Monte Carlo
+#' simulations of C thicknesses between 0 and 100 nm on Si at 10 kV
+#'
+#' @param c.to.si Ratio of C to Si peak intensities at 10 kV.
+#'
+#' @param verbose Boolean flag. Default FALSE. Print info if TRUE.
+#'
+#' @return The C thickness in nm
+#'
+#' @keywords keywords
+#'
+#' @examples
+#'
+#' library(rEDS)
+#' a <- computeCarbonThicknessOnSi10kV(0.07)
+#' print(a)
+#'
+#' @export
+computeCarbonThicknessOnSi10kV <- function(c.to.si, verbose=FALSE){
+  t.C.nm <- seq(from=0.5, to=100, by=0.5)
+  df2 <- data.frame(t.C.nm=t.C.nm, c.to.si.mu=t.C.nm)
+  fi <- system.file("extdata", "c-to-si-loess-10kV.rda", package = "rEDS")
+  load(fi)
+  if(verbose) print(ls())
+  df2$c.to.si.mu <- predict(c.to.si.loess.10kV, newdat=df2)
+  df2$dif <- sqrt((df2$c.to.si.mu-c.to.si)^2)
+  mv = min(df2$dif)
+  t <- df2$t.C.nm[which(df2$dif==mv)]
+  return(t)
+}
---
+++ R/computeCarbonThicknessOnSi20kV.R
@@ -0,0 +1,33 @@
+#' Compute the C thickness on Si at 20kV
+#'
+#' Use the results from a LOESS fit to results from DTSA-II Monte Carlo
+#' simulations of C thicknesses between 0 and 100 nm on Si at 20 kV
+#'
+#'
+#' @param c.to.si Ratio of C to Si peak intensities at 20 kV.
+#'
+#' @param verbose Boolean flag. Default FALSE. Print info if TRUE.
+#'
+#' @return The C thickness in nm
+#'
+#' @keywords keywords
+#'
+#' @examples
+#'
+#' library(rEDS)
+#' a <- computeCarbonThicknessOnSi20kV(0.0125)
+#' print(a)
+#'
+#' @export
+computeCarbonThicknessOnSi20kV <- function(c.to.si, verbose=FALSE){
+  t.C.nm <- seq(from=0.5, to=100, by=0.5)
+  df2 <- data.frame(t.C.nm=t.C.nm, c.to.si.mu=t.C.nm)
+  fi <- system.file("extdata", "c-to-si-loess-20kV.rda", package = "rEDS")
+  load(fi)
+  if(verbose) print(ls())
+  df2$c.to.si.mu <- predict(c.to.si.loess.20kV, newdat=df2)
+  df2$dif <- sqrt((df2$c.to.si.mu-c.to.si)^2)
+  mv = min(df2$dif)
+  t <- df2$t.C.nm[which(df2$dif==mv)]
+  return(t)
+}
