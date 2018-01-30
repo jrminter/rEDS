fabdd0eed49ac15c8da7d21c39a7a45c12a4c2cd
John Minter <jrminter@gmail.com>
2018-01-30 13:38:36 -0500

    Fixed penepmaToMsa. Switched to 1 col and eV from keV. Bumped the version...

--- R/penepmaToMsa.R
+++ R/penepmaToMsa.R
@@ -11,7 +11,7 @@
 #'
 #' @param e0 number - the voltage in kV for the simulation
 #'
-#' @param title string - label for the spectrum
+#' @param title string - label for the spectrum (default "Penepma")
 #'
 #' @param owner string - label for the scientist
 #'
@@ -28,30 +28,50 @@
 #' # Not run
 #' # inF <- '../penepma/pe-spect-01.dat'
 #' # ouF <- '../penepma/k411-15kV.msa'
-#' # penepmaToMsa(inF, ouF, 15, 'simulation', 'jrminter')
+#' # penepmaToMsa(inF, ouF, 15, 'simulation')
 #'
 #' @export
 penepmaToMsa <- function(datFile,
                            msaFile,
                            e0,
                            title,
-                           owner,
+                           owner="Penepma",
                            bDebug=FALSE){
-    df <- read.table(datFile, header = FALSE, sep=" ", skip=12)
+    df <- read.table(inF, header = FALSE, sep=" ", skip=12)
     df <- df[, c(4,6,8)]
-    keV <- df[,1]/1000.
-    df[,1] <- keV
-    names(df) <- c('keV', 'pd', 'unc')
-    df <- df[df$pd > 1.0e-35, ]
-    mv <- min(df$pd)
+
+    names(df) <- c('eV', 'pd', 'unc')
+
+    setLoToNA <- function(x){
+        if (x == 1.0e-35){
+            x <- NA
+        }
+        x
+    }
+
+    df$pd  <- as.numeric(lapply(df$pd, setLoToNA))
+    df$unc <- as.numeric(lapply(df$unc, setLoToNA))
+
+    mv <- min(df$pd, na.rm = TRUE)
     df$pd <- df$pd/mv
     df$unc <- df$unc/mv
+
+    mv <- min(df$pd, na.rm = TRUE)
+    df$pd[is.na(df$pd)]  <- mv  # <- 1.0
+
+    mv <- min(df$unc, na.rm = TRUE)
+    df$unc[is.na(df$unc)] <- mv # 5.0
+
     rownames(df) <- c()
+    d <- diff(df$eV)
+    deltaE <- median(d)
 
 
     if(bDebug == TRUE){
-      print(head(df))
-      print(tail(df))
+        print(head(df))
+        print(tail(df))
+        print("ev/ch")
+        print(deltaE)
     }
 
     sink(msaFile)
@@ -69,24 +89,20 @@ penepmaToMsa <- function(datFile,
     cat(li)
     li <- sprintf('#OWNER       : %s\n', owner)
     cat(li)
-    npts <- length(df$keV)
-    li <- sprintf('#NPOINTS     : %s\n', npts-2)
+    npts <- length(df$eV)
+    li <- sprintf('#NPOINTS     : %s\n', npts)
     cat(li)
-    cat('#NCOLUMNS    : 2\n')
+    cat('#NCOLUMNS    : 1\n')
     cat('#XUNITS      : eV\n')
     cat('#YUNITS      : counts\n')
-    cat('#DATATYPE    : XY\n')
+    cat('#DATATYPE    : Y\n')
 
-    eStart <- 1000.0*df$keV[1]
-    eEnd <- 1000.0*df$keV[nrow(df)]
-    deltaE <- (eEnd-eStart)/(as.numeric(nrow(df)-1.0))
-
-    li <- sprintf('#XPERCHAN    : %.2f\n', deltaE)
+    li <- sprintf('#XPERCHAN    : %.3f\n', deltaE)
     cat(li)
 
-    xo <- sprintf("#OFFSET      : %.2f\n", eStart)
-    cat(xo)
-    # cat("#OFFSET      : 0.0\n")
+    # xo <- sprintf("#OFFSET      : %.2f\n", eStart)
+    # cat(xo)
+    cat("#OFFSET      : 0.0\n")
 
     ev <- sprintf('#BEAMKV      : %g\n', e0)
     cat(ev)
@@ -101,7 +117,8 @@ penepmaToMsa <- function(datFile,
     while(i < lData){
       eV <- round(1000.0*df$keV[i])
       cts <- round(df$pd[i], 1)
-      li <- sprintf('%.1f, %.1f \n', eV, cts)
+      # li <- sprintf('%.1f, %.1f \n', eV, cts)
+      li <- sprintf('%.1f \n', cts)
       cat(li)
       i = i + 1
     }
